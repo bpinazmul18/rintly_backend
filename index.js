6557c3afe18743f53173eedd021205f1e820fa2d
Nazmul Haque <bpinazmul18@gmail.com>
2022-03-13 21:52:18 +0600

    Remove extra line of code.

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const debug = require('debug')('app:startup')
-const config = require('config')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const express = require('express')
-const logger = require('./middleware/logger')
-const auth = require('./middleware/auth')
 const app = express()
 const genres = require('./routes/genres')
 const home = require('./routes/home')
 
 const port = process.env.PORT || 3001
-
-console.log(`Node env: ${process.env.NODE_ENV}`)
-console.log(`NODE_ENV: ${app.get('env')}`)
 
 // Middleware
 app.set('view engine', 'pug')
@@ -23,32 +17,15 @@ app.use(express.urlencoded({ extended: true}))
 app.use(helmet())
 app.use(express.static('public'))
 
-
+// Logger
 if(app.get('env') === 'development') {
     app.use(morgan('tiny'))
     debug('Morgan enable...')
 }
 
-
-
-// practice middleware
-// app.use(logger)
-// app.use(auth)
-
-
 // Routes
 app.use('/', home)
 app.use('/api/genres', genres)
-
-
-
-
-
-// Configuration
-console.log('Application name: ' + config.get('name'))
-console.log('Application mail server: ' + config.get('mail.host'))
-console.log("Application mail password: " + config.get('mail.password'))
-
 
 app.listen(port, ()=> console.log(`App listening on port http://localhost:${port}`))
 
