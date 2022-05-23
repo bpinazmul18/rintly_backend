9d9d1c23e57b953f43c368f1c2d1e330970e80d6
Nazmul Haque <bpinazmul18@gmail.com>
2022-05-23 19:26:52 +0600

    Practice config module.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const debug = require('debug')('app:startup')
+const config = require('config')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const express = require('express')
 const app = express()
+
+/**
+ * Routers
+ */
 const genres = require('./routes/genres')
 const home = require('./routes/home')
 
@@ -17,6 +22,8 @@ app.use(express.urlencoded({ extended: true}))
 app.use(helmet())
 app.use(express.static('public'))
 
+console.log(config.get('mail.password'))
+
 // Logger
 if(app.get('env') === 'development') {
     app.use(morgan('tiny'))
@@ -28,7 +35,3 @@ app.use('/', home)
 app.use('/api/genres', genres)
 
 app.listen(port, ()=> console.log(`App listening on port http://localhost:${port}`))
-
-
-
-
