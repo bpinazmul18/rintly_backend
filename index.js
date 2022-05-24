c9e735880362513fa316ca6f4a2b641929fbf24b
Nazmul Haque <bpinazmul18@gmail.com>
2022-05-24 07:08:18 +0600

    Querying documents.

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ const Course = mongoose.model('Course', courseSchema)
 
 async function createCourse () {
     const course = new Course({
-        name: 'Nodejs course',
-        author: 'Nazmul',
-        tags: ['node', 'backend'],
+        name: 'Angular course',
+        author: 'Rabu',
+        tags: ['angular', 'frontend'],
         isPublished: true
     })
     
@@ -60,9 +60,17 @@ async function createCourse () {
     console.log(`Successfully save new course: ${result}`)
 }
 
-createCourse()
+async function getCourses () {
+    const courses = await Course
+        .find({ author: 'Rabu', isPublished: true})
+        .limit(10)
+        .sort({ name: 1})
+        .select({ name: 1, tags: 1})
 
+    console.log(courses)
+}
 
+getCourses()
 
 // Routes
 app.use('/', home)
