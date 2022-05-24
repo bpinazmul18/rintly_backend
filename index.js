bceb48de6ec2f93a0b803cdd8343dcbe42acc5d4
Nazmul Haque <bpinazmul18@gmail.com>
2022-05-24 07:36:28 +0600

    Logical Query Operators

--- index.js
+++ index.js
@@ -70,14 +70,20 @@ async function getCourses () {
     // in
     // nin (not in)
 
+    // or
+    // and
+
     const courses = await Course
         // .find({ author: 'Rabu', isPublished: true})
         // .find({ price: 10 })
         // .find({ price: { $gte: 10, $lte: 20 }})
-        .find({ price: { $in: [ 10, 15, 20 ]}})
+        // .find({ price: { $in: [ 10, 15, 20 ]}})
+        .find()
+        .or([{ author: 'Nazmul' }, { isPublished: false }])
+        .and([{}])
         .limit(10)
         .sort({ name: 1})
-        .select({ name: 1, tags: 1})
+        .select({ name: 1, tags: 1, author: 1})
 
     console.log(courses)
 }
