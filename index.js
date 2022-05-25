ad063d9fd5e7d764b2cc8aca351c39fadaaa60b1
Nazmul haque <bpinazmul18@gmail.com>
2022-05-25 12:08:48 +0600

    Custom validator.

--- index.js
+++ index.js
@@ -52,7 +52,15 @@ const courseSchema = new mongoose.Schema({
         enum: ['web', 'mobile', 'network']
     },
     author: String,
-    tags: [ String ],
+    tags: {
+      type: Array,
+      validate: {
+          validator: function (v) {
+              return v && v.length > 0
+          },
+          message: 'A course should have at list one tag.'
+      }
+    },
     date: {
         type: Date,
         default: Date.now()
@@ -71,9 +79,10 @@ const Course = mongoose.model('Course', courseSchema)
 async function createCourse () {
     const course = new Course({
         name: 'Node course',
-        category: '-',
+        category: 'web',
         author: 'Rabu',
-        tags: ['node', 'backend'],
+        // tags: [],
+        tags: null,
         isPublished: true,
         price: 50
     })
