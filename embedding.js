5a583b160b5d3be0270f3fde55411f09f0b17435
Nazmul Haque <bpinazmul18@gmail.com>
2022-05-30 19:10:19 +0600

    Using array of sub documents.

--- embedding.js
+++ embedding.js
@@ -14,16 +14,27 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: {
-      type: authorSchema,
-      required: true,
-  }
+  // author: {
+  //     type: authorSchema,
+  //     required: true,
+  // }
+  authors: [authorSchema]
 }));
 
-async function createCourse(name, author) {
+// async function createCourse(name, author) {
+//   const course = new Course({
+//     name, 
+//     author
+//   }); 
+  
+//   const result = await course.save();
+//   console.log(result);
+// }
+
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -36,7 +47,7 @@ async function listCourses() {
 }
 
 async function updateCourse(courseId){
-  const course = await Course.update({ _id: courseId }, {
+  const course = await Course.updateOne({ _id: courseId }, {
     $unset: {
       'author': ''
     }
@@ -44,16 +55,28 @@ async function updateCourse(courseId){
 }
 
 async function addAuthor(courseId, author){
-  const course = await Course.findById(courseId);
-  course.authors.push(author);
-  console.log(await course.save());
+  // const course = await Course.findById(courseId);
+  // course.authors.push(author);
+  // console.log(await course.save());
+
+  const course = await Course.findById(courseId)
+  course.authors.push(author)
+
+  course.save()
+
+  console.log(course);
 }
 
 async function removeAuthor(courseId, authorId){
-  const course = await Course.findById(courseId);
-  var theAuthor = course.authors.id(authorId);
-  theAuthor.remove();
-  console.log(await course.save());
+  // const course = await Course.findById(courseId);
+  // var theAuthor = course.authors.id(authorId);
+  // theAuthor.remove();
+  // console.log(await course.save());
+
+  const course = await Course.findById(courseId)
+  const author = course.authors.id(authorId)
+  author.remove()
+  course.save()
 }
 
 async function updateAuthor (courseId) {
@@ -78,5 +101,14 @@ async function updateAuthor (courseId) {
     console.log(`Course ${course}`);
 }
 
+
+
 // createCourse('Node Course', new Author({ name: 'Nazmul Haque'})); 
-updateAuthor('62922489a328a513d5919744')
+// createCourse('Node Course', [
+//   new Author({ name: 'Nazmul Haque'}),
+//   new Author({ name: 'Borsha'})
+// ]); 
+// updateAuthor('62922489a328a513d5919744')
+
+// addAuthor('62937ca69a8bd84428c84901', new Author({ name: 'Johan'}))
+removeAuthor('62937ca69a8bd84428c84901', '62937d5e1d41d28e0072507c')
