a91c4cfeda9349322afc10861b1219f682362f69
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-03 14:55:19 +0600

    Get single movie, delete movie and remove movie.

--- routes/movies.js
+++ routes/movies.js
@@ -11,7 +11,7 @@ router.get('/', async(req, res) => {
 
         return res.send(movies)
     } catch(ex) {
-        return res.status(500).send('Server error!', ex.message)
+        return res.status(500).send(`Server error! ${ex.message}`)
     }
     
 })
@@ -21,25 +21,52 @@ router.post('/', async (req, res) => {
     const { error, value } = validate(req.body)
     if(error) return res.status(400).send(error['details'][0].message)
 
-    // Find by genreId
-    const genre = await Genre.findById(value['genreId'])
-    if(!genre) return res.status(400).send('Invalid genre.')
-
-    // New movie
-    let movie = new Movie({
-        title: value['title'],
-        genre: {
-            _id: genre._id,
-            name: genre.name
-        },
-        numberInStock: value['numberInStock'],
-        dailyRentalRate: value['dailyRentalRate']
-    })
-
-    // Save to database and return to client
     try {
+        // Find by genreId
+        const genre = await Genre.findById(value['genreId'])
+        if(!genre) return res.status(400).send('Invalid genre.')
+
+        // New movie
+        let movie = new Movie({
+            title: value['title'],
+            genre: {
+                _id: genre._id,
+                name: genre.name
+            },
+            numberInStock: value['numberInStock'],
+            dailyRentalRate: value['dailyRentalRate']
+        })
+
+        // Save to database and return to client
         const result = await movie.save()
         return res.send(result)
+    } catch (ex) {
+        return res.status(500).send(`Server error! ${ex.message}`)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        // Find user by ID
+        const movie = await Movie.findById(req.params.id)
+        if(!movie) return res.status(404).send('movie was not found by given ID!')
+
+        // Response to the client
+        return res.send(movie)
+
+    } catch (ex) {
+        return res.status(500).send(`Server error! ${ex.message}`)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        // Find user by ID
+        const movie = await Movie.findByIdAndRemove(req.params.id)
+        if(!movie) return res.status(404).send('movie was not found by given ID!')
+
+        // Response to the client
+        return res.send(movie)
     } catch (ex) {
         return res.status(500).send(`Server error! ${ex.message}`)
     }
