1d8a514804042c1ceed92e8315a4184cac66ee9f
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-03 14:38:00 +0600

    Bug fixed home routes.

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('index', {title: 'My express app.', message: 'Hello world'})
+router.get('/', async (req, res) => {
+    try {
+        res.render('index', {title: 'My express app.', message: 'Hello world'})
+    } catch (ex) {
+        return res.status(500).send(`Server error! ${ex.message}`)
+    }
 })
 
 module.exports = router
