59446ef45dafc9429292a804ac65fd45e41289b5
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-03 14:47:01 +0600

    Genres api refactoring.

--- routes/genres.js
+++ routes/genres.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
         const genres = await Genre.find().sort('name')
         return res.send(genres)
     } catch (ex) {
-        return res.status(500).send('Server error!', ex.message)
+        return res.status(500).send(`Server error! ${ex.message}`)
     }
 })
 
@@ -24,7 +24,7 @@ router.post('/', async (req, res) => {
         genre = await genre.save()
         return res.send(genre)
     } catch (ex) {
-        return res.status(500).send('Server error!', ex.message)
+        return res.status(500).send(`Server error! ${ex.message}`)
     }
 })
 
@@ -33,29 +33,42 @@ router.put("/:id", async (req, res) => {
     const {error, value} = validate(req.body)
     if(error) return res.status(400).send(error['details'][0].message)
 
-    // Find genre by ID and update
-    const genre = await Genre.findByIdAndUpdate(req.params.id, value, { new: true})
-    if(!genre) return res.status(404).send('genre was not found by given ID!')
+    try {
+        // Find genre by ID and update
+        const genre = await Genre.findByIdAndUpdate(req.params.id, value, { new: true})
+        if(!genre) return res.status(404).send('genre was not found by given ID!')
 
-    // Response to the client
-    return res.send(genre)
+        // Response to the client
+        return res.send(genre)
+    } catch (ex) {
+        return res.status(500).send(`Server error! ${ex.message}`)
+    }
 })
 
 router.get('/:id', async (req, res) => {
-    // Find user by ID
-    const genre = await Genre.findById(req.params.id)
-    if(!genre) return res.status(404).send('genre was not found by given ID!')
+    try {
+        // Find user by ID
+        const genre = await Genre.findById(req.params.id)
+        if(!genre) return res.status(404).send('genre was not found by given ID!')
 
-    return res.send(genre)
+        // Response to the client
+        return res.send(genre)
+    } catch (ex) {
+        return res.status(500).send(`Server error! ${ex.message}`)
+    }
 })
 
 router.delete('/:id', async (req, res) => {
-    // Find user by ID
-    const genre = await Genre.findByIdAndRemove(req.params.id)
-    if(!genre) return res.status(404).send('genre was not found by given ID!')
+    try {
+        // Find user by ID
+        const genre = await Genre.findByIdAndRemove(req.params.id)
+        if(!genre) return res.status(404).send('genre was not found by given ID!')
 
-    // Response to the client
-    return res.send(genre)
+        // Response to the client
+        return res.send(genre)
+    } catch (ex) {
+        return res.status(500).send(`Server error! ${ex.message}`)
+    }
 })
 
 module.exports = router
