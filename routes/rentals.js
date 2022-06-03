00ac411db490a000bbc89f0407f54351c932ec38
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-03 14:58:35 +0600

    Rentals refactoring.

--- routes/rentals.js
+++ routes/rentals.js
@@ -10,8 +10,6 @@ const { validate, Rental } = require('../models/rental')
 
 Fawn.init(config.get('dbURI'))
 
-
-
 router.get('/', async(req, res) => {
     try {
         const rentals = await Rental.find()
@@ -20,7 +18,7 @@ router.get('/', async(req, res) => {
 
         return res.send(rentals)
     } catch(ex) {
-        return res.status(500).send('Server error!', ex.message)
+        return res.status(500).send(`Server error! ${ex.message}`)
     }
 })
 
@@ -29,35 +27,32 @@ router.post('/', async(req, res) => {
     const {error, value} = validate(req.body)
     if(error) return res.status(400).send(error['details'][0].message)
 
-    const customer = await Customer.findById(value['customerId'])
-    if (!customer) return res.status(400).send('Invalid customer!')
 
-    const movie = await Movie.findById(value['movieId'])
-    if (!movie) return res.status(400).send('Invalide movie!')
-
-    if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock!')
-
-    // New rental
-    let rental = new Rental({
-        customer: {
-            _id: customer._id,
-            name: customer.name,
-            phone: customer.phone
-        },
-        movie: {
-            _id: movie._id,
-            title: movie.title,
-            dailyRentalRate: movie.dailyRentalRate
-        }
-    })
 
     // Save to database and return to client
     try {
-        // result = await rental.save()
-
-        // movie.numberInStock--
-        // movie.save()
-
+        const customer = await Customer.findById(value['customerId'])
+        if (!customer) return res.status(400).send('Invalid customer!')
+    
+        const movie = await Movie.findById(value['movieId'])
+        if (!movie) return res.status(400).send('Invalide movie!')
+    
+        if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock!')
+    
+        // New rental
+        let rental = new Rental({
+            customer: {
+                _id: customer._id,
+                name: customer.name,
+                phone: customer.phone
+            },
+            movie: {
+                _id: movie._id,
+                title: movie.title,
+                dailyRentalRate: movie.dailyRentalRate
+            }
+        })
+        
         new Fawn.Task()
             .save('rentals', rental)
             .update('movies', {_id: movie._id}, {
