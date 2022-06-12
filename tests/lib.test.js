24b0d8f8e96647eac652acbc88a615060c7e6322
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-12 19:53:18 +0600

    Tesing numbers.

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -4,3 +4,13 @@ test('Absolute- should return a positive number if input is positive.', () => {
     const result = lib(1)
     expect(result).toBe(1)
 })
+
+test('Absolute- should return a positive number if input is Negative.', () => {
+    const result = lib(-1)
+    expect(result).toBe(1)
+})
+
+test('Absolute- should return a positive number if input is 0.', () => {
+    const result = lib(0)
+    expect(result).toBe(1)
+})
