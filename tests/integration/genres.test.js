0fe0495d23bd972bf23ebc1fc050c4c2a915eb4b
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-15 07:27:17 +0600

    Populate the test db.

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest')
+const { Genre } = require('../../models/genre')
 let server;
 
 
@@ -7,14 +8,23 @@ describe('/api/genres', () => {
         server = require('../../index')
     })
 
-    afterEach(() => {
+    afterEach( async () => {
         server.close()
+        await Genre.remove({})
     })
 
     describe('GET /', () => {
         it('should return all genres.', async () => {
+            await Genre.collection.insertMany([
+                {name: 'genre1'},
+                {name: 'genre2'}
+            ])
+
             const res = await request(server).get('/api/genres')
             expect(res.status).toBe(200)
+            expect(res.body.length).toBe(2)
+            expect(res.body.some((g) => g.name === 'genre1')).toBeTruthy()
+            expect(res.body.some((g) => g.name === 'genre2')).toBeTruthy()
         })
     })
 })
