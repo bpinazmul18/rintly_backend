8674ead782e585127c6446c15898ed3c6521c51f
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-15 07:42:30 +0600

    Testing routes with parameters.

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -27,4 +27,15 @@ describe('/api/genres', () => {
             expect(res.body.some((g) => g.name === 'genre2')).toBeTruthy()
         })
     })
+
+    describe('GET /:id', () => {
+        it('should return a genre if valid id is passed.', async () => {
+            const genre = await new Genre({ name: 'genre1'})
+            await genre.save()
+
+            const res = await request(server).get(`/api/genres/${genre._id}`)
+            expect(res.status).toBe(200)
+            expect(res.body).toHaveProperty('name', genre.name)
+        })
+    })
 })
