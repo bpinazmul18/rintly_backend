ca726a6383b6205a887845215fc468ff1929a0d6
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-18 06:13:08 +0600

    Testing auth middleware.

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,46 @@
+const request = require('supertest')
+const { User } = require('../../models/user')
+const { Genre } = require('../../models/genre')
+
+
+let server
+
+describe('auth middleware.', () => {
+    let token
+    let name
+
+    beforeEach(() => { 
+        server = require('../../index')
+        token = new User().generateAuthToken()
+    })
+
+    afterEach( async () => { 
+        await Genre.deleteMany({})
+        await server.close() 
+    })
+
+    const exec = () => {
+        return request(server)
+            .post('/api/genres')
+            .set('x-auth-token', token)
+            .send({ name })
+    }
+
+    it ('should return 401 if no token is provided.', async () => {
+        token = ''
+        const res = await exec()
+        expect(res.status).toBe(401)
+    })
+
+    it ('should return 400 if token is invalid.', async () => {
+        token = 'a'
+        const res = await exec()
+        expect(res.status).toBe(400)
+    })
+
+    it ('should return 200 if token is valid.', async () => {
+        name = 'genre1'
+        const res = await exec()
+        expect(res.status).toBe(200)
+    })
+})
