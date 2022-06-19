efc3b516b933eb061e5068d096e117e7c2f2e250
Nazmul haque <bpinazmul18@gmail.com>
2022-06-19 16:10:57 +0600

    Fixed openHandles log errors.

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -20,7 +20,9 @@ describe('/api/genres', () => {
                 {name: 'genre2'}
             ])
 
-            const res = await request(server).get('/api/genres')
+            const res = await request(server)
+                .get('/api/genres')
+
             expect(res.status).toBe(200)
             expect(res.body.length).toBe(2)
             expect(res.body.some((g) => g.name === 'genre1')).toBeTruthy()
