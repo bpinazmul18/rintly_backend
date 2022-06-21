bfe782c06093c205aa01813f4e8695ee69876538
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-21 06:05:43 +0600

    Testing the input.

--- routes/returns.js
+++ routes/returns.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
+    if (!req.body.customerId) return res.status(400).send('customerId is not provided.')
+    if (!req.body.movieId) return res.status(400).send('movieId is not provided.')
     return res.status(401).send('Unauthorized')
 })
 
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const { mongoose } = require('mongoose')
 const { Rental } = require('../../models/rental')
+const { User } = require('../../models/user')
 
 describe('/api/returns', () => {
     let server
@@ -41,5 +42,27 @@ describe('/api/returns', () => {
             .send({ customerId, movieId})
 
         expect(res.status).toBe(401)
+    })
+
+    it('should return 400 if customerId is not provided!', async () => {
+        const token = new User().generateAuthToken()
+
+        const res = await request(server)
+            .post('/api/returns')
+            .set('x-auth-token', token)
+            .send({ movieId })
+
+        expect(res.status).toBe(400)
+    })
+
+    it('should return 400 if movieId is not provided!', async () => {
+        const token = new User().generateAuthToken()
+
+        const res = await request(server)
+            .post('/api/returns')
+            .set('x-auth-token', token)
+            .send({ customerId })
+
+        expect(res.status).toBe(400)
     })
 })
