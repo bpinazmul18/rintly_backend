0475a08d943e0ef97ddd3ce15a6abe9d0f34302a
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-26 07:08:34 +0600

    Add cros policy.

--- startup/prod.js
+++ startup/prod.js
@@ -1,7 +1,9 @@
+const cros = require('cros')
 const helmet = require('helmet')
 const compression = require('compression')
 
 const prod = (app) => {
+    app.use(cros())
     app.use(helmet())
     app.use(compression())
 }
